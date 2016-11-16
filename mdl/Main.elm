497db5c6a43937f3fa36fa0ad949950b69f5a635
Rob Ashton <robashton@codeofrob.com>
2016-11-16 11:40:12 +0000

    Mdl demo

--- mdl/Main.elm
+++ mdl/Main.elm
@@ -6,23 +6,39 @@ import Model exposing (..)
 import Api
 import Task
 import Http
+import Material
+import Material.Button as Button
+import Material.Scheme
+import Material.Color as Color
+import Material.Card as Card
+import Material.Options as Options exposing (css, when)
+import Material.Typography as Typography
+import Material.Layout as Layout
+import Material.Helpers exposing (lift)
+import Material.Grid exposing (grid, cell, size, offset, Device(..))
 
 
 type alias Model =
     { posts : Maybe (List Post)
+    , mdl : Material.Model
     }
 
 
 type Msg
     = HttpFail Http.Error
+    | Mdl (Material.Msg Msg)
     | PostsLoaded (List Post)
 
 
 init : ( Model, Cmd Msg )
 init =
     ( { posts = Nothing
+      , mdl = Material.model
       }
-    , loadPosts
+    , Cmd.batch
+        [ loadPosts
+        , Material.init Mdl
+        ]
     )
 
 
@@ -33,6 +49,9 @@ loadPosts =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        Mdl msg' ->
+            Material.update msg' model
+
         PostsLoaded posts ->
             ( { model | posts = Just posts }, Cmd.none )
 
@@ -42,8 +61,38 @@ update msg model =
                 ( model, Cmd.none )
 
 
-view : Model -> Html msg
+view : Model -> Html Msg
 view model =
+    Material.Scheme.topWithScheme Color.Teal Color.Red
+        <| Layout.render Mdl
+            model.mdl
+            [ Layout.fixedHeader
+            ]
+            { header = header model
+            , drawer = []
+            , tabs = ( [], [] )
+            , main = [ mainView model ]
+            }
+
+
+header : Model -> List (Html Msg)
+header model =
+    [ Layout.row []
+        [ Layout.title [] [ text "Cool BuildStuff Demo" ]
+        , Layout.spacer
+        , div []
+            [ Layout.navigation []
+                [ Layout.link [ Layout.href "http://buildstuff.lt" ]
+                    [ div [] [ text "BuildStuff" ] ]
+                ]
+            ]
+        , div [] []
+        ]
+    ]
+
+
+mainView : Model -> Html Msg
+mainView model =
     case model.posts of
         Nothing ->
             p [] [ text "Loading.." ]
